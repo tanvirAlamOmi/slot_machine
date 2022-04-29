f56fc390feb923c537cd08056bd462440a24ad15
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-29 12:57:42 +0600

    mysql config setup

--- admin/server/src/app.module.ts
+++ admin/server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './model/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [],
+    // synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
