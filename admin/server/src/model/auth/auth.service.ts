d774df6653775dcf1761bab07823afbca6a0a3b9
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-05-01 03:58:09 +0600

    auth signup & login setup

--- admin/server/src/model/auth/auth.service.ts
+++ admin/server/src/model/auth/auth.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersSerive: UsersService
+  ){}
+
+  async validateUser(loginDto: LoginDto): Promise<User> {
+    const { email, password } = loginDto;     
+
+    const user = await this.usersSerive.findOneByEmail(email);
+
+    if (! user) throw new UnauthorizedException("email or password don't match");
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (! isPasswordMatch) throw new UnauthorizedException("email or password don't match");
+     
+    if(!user.isActive) throw new UnauthorizedException("User not permitted to access");
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  signup(signupDto: SignupDto) {
+    return this.usersSerive.create(signupDto);
+  }
+  
+  async login(loginDto: LoginDto) {
+    return await this.validateUser(loginDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
