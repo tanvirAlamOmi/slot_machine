84648e60f3e7b13c05b765e620d3e3cccffdc816
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-05-01 03:58:56 +0600

    dtos added

---
+++ admin/server/src/model/auth/dto/signup.dto.ts
@@ -0,0 +1,18 @@
+import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class SignupDto {
+
+    @IsString()
+    username: string;
+    
+    @IsString()    
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsBoolean()
+    @IsOptional()
+    isActive: boolean;
+}
