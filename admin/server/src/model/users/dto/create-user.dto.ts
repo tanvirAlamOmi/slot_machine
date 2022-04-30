0bc86a3035ae6f639cd86238173ce2c58eb42143
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-30 15:10:04 +0600

    validation setup

--- admin/server/src/model/users/dto/create-user.dto.ts
+++ admin/server/src/model/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    username: string;
+    
+    @IsString()    
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsBoolean()
+    @IsOptional()
+    isActive: boolean;
+}
