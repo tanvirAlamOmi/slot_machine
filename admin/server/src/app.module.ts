a4c92274af4b32f9ef407c9477dabbc2dc959f85
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-05-01 03:57:32 +0600

    auth module registered

--- admin/server/src/app.module.ts
+++ admin/server/src/app.module.ts
@@ -3,22 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './model/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './model/auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot({
-    // type: 'mysql',
-    type: 'sqlite',
-    // host: 'localhost',
-    // port: 3306,
-    // username: 'root',
-    // password: 'root',
-    database: 'arbree_slot',
-    entities: [__dirname + "/**/*.entity{.ts,.js}"],
-    // entities: [],
-    synchronize: true,
-    // synchronize: true, //remove at production
-  })],
+  imports: [UsersModule, AuthModule, 
+    TypeOrmModule.forRoot({
+      // type: 'mysql',
+      type: 'sqlite',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'root',
+      database: 'arbree_slot',
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      // entities: [],
+      synchronize: true,
+      // synchronize: true, //remove at production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
