571ed0957134772975bfa51ce3384dedec41d82c
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-30 15:09:08 +0600

    response interceptor created

--- admin/server/src/model/users/users.module.ts
+++ admin/server/src/model/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
-@Module({
+
+@Module({  
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, 
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    }
+  ]
 })
 export class UsersModule {}
