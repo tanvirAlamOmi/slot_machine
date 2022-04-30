c68d2cf5da0c27dc8fa6908588d681f0930238c3
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-30 15:11:00 +0600

    user schema created

--- admin/server/src/model/users/entities/user.entity.ts
+++ admin/server/src/model/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Exclude } from 'class-transformer'
+import { IsDate, Length } from 'class-validator'
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    @Length(4, 10)
+    username: string;
+
+    @Column()
+    email: string;
+ 
+    @Column()
+    @Exclude()
+    password: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+ 
+    @CreateDateColumn()
+    @IsDate()
+    createdAt : Date;
+ 
+    @UpdateDateColumn()
+    @IsDate()
+    updtedAt : Date;
+
+}
