dcd2360bf0ebf609227989072e45f869136ae5b2
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-30 15:09:29 +0600

    sqlite3 setup

--- admin/server/src/app.module.ts
+++ admin/server/src/app.module.ts
@@ -7,14 +7,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [],
-    // synchronize: true,
+    // type: 'mysql',
+    type: 'sqlite',
+    // host: 'localhost',
+    // port: 3306,
+    // username: 'root',
+    // password: 'root',
+    database: 'arbree_slot',
+    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    // entities: [],
+    synchronize: true,
+    // synchronize: true, //remove at production
   })],
   controllers: [AppController],
   providers: [AppService],
