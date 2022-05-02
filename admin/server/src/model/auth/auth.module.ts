14d0371ed521a2cf9115bb9d9d30fea9c16c08cf
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-05-02 20:26:06 +0600

    passport & jwt setup

--- admin/server/src/model/auth/auth.module.ts
+++ admin/server/src/model/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { AccessTokenjwtStrategy } from './strategies/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, 
+    PassportModule, 
+    JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AccessTokenjwtStrategy]
 })
 export class AuthModule {}
